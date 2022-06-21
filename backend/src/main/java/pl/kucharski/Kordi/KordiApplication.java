68734e3aa38dbbeadb021268754adff4a3b44f61
Gelo <gelo2424@wp.pl>
2022-06-21 21:41:25 +0200

    backend initial commit

---
+++ backend/src/main/java/pl/kucharski/Kordi/KordiApplication.java
@@ -0,0 +1,13 @@
+package pl.kucharski.Kordi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KordiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KordiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/kucharski/Kordi/KordiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kucharski.Kordi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KordiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
